efe2312617c54e215b032d0ab3fe40a4b2be6006
lgdd <louis-guillaume.durand@liferay.com>
2020-09-17 09:36:05 +0200

    :recycle: Refactoring to separate Gradle daemon from Liferay runtime

---
+++ start-liferay.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+function main {
+	if [ "${LIFERAY_JPDA_ENABLED}" == "true" ]
+	then
+		${LIFERAY_HOME}/tomcat-9.0.17/bin/catalina.sh jpda run
+	else
+		${LIFERAY_HOME}/tomcat-9.0.17/bin/catalina.sh run
+	fi
+}
+
+main
